ddacdfe08cb2614dc8bf28978cee80fc05028b50
DCurro <domenic.curro@gmail.com>
2016-08-06 16:58:18 -0400

    Attempting to read from local file.

--- main.js
+++ main.js
@@ -16,5 +16,26 @@ buttonReddenDiv.onmouseup = function () {
     div.style.backgroundColor = "blue";
 }
 
+result = httpGet("example.txt")
 
-
+function httpGet(theUrl)
+{
+    if (window.XMLHttpRequest)
+    {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    }
+    else
+    {// code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function()
+    {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+            alert(xmlhttp.responseText)
+            return xmlhttp.responseText;
+        }
+    }
+    xmlhttp.open("GET", theUrl, false );
+    xmlhttp.send();
+}
